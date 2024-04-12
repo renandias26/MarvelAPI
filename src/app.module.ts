a0e5d3faf9e8516003ba3809e544c2005eaae0f2
Renan Dias <renanrsd26.rd@gmail.com>
2024-04-11 22:02:37 -0300

    Add/Adiciona modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ComicsModule } from './comics/comics.module';
+import { CharactersModule } from './characters/characters.module';
+import { CreatorsModule } from './creators/creators.module';
 
 @Module({
-  imports: [],
+  imports: [ComicsModule, CharactersModule, CreatorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
