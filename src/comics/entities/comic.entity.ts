f5dd36579522ec57b23b7fc91f283ae6001e2e06
Renan Dias <renanrsd26.rd@gmail.com>
2024-04-12 13:57:32 -0300

    Add/Adiciona entidades

--- src/comics/entities/comic.entity.ts
+++ src/comics/entities/comic.entity.ts
@@ -1,1 +1,24 @@
-export class Comic {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type ComicDocument = HydratedDocument<Comic>;
+
+@Schema({ timestamps: true })
+export class Comic {
+    @Prop({ require: true })
+    title: string;
+
+    @Prop({ require: false })
+    pageCount: number
+
+    @Prop({ require: false })
+    format: String
+
+    @Prop({ require: true })
+    imagePath: String
+
+    @Prop({ require: true })
+    idApi: number
+}
+
+export const ComicSchema = SchemaFactory.createForClass(Comic);
--- src/creators/entities/creator.entity.ts
+++ src/creators/entities/creator.entity.ts
@@ -1,1 +1,21 @@
-export class Creator {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type CreatorDocument = HydratedDocument<Creator>;
+
+@Schema({ timestamps: true })
+export class Creator {
+    @Prop({ require: true })
+    fullName: string;
+
+    @Prop({ require: false })
+    role: String
+
+    @Prop({ require: true })
+    imagePath: String
+
+    @Prop({ require: true })
+    idApi: number
+}
+
+export const CreatorSchema = SchemaFactory.createForClass(Creator);
